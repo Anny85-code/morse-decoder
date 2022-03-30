5a7632ca22efef78d52f515aedc3569fd4b80f22
clintonjosephs <clintonjosephs26@gmail.com>
2022-03-30 14:47:40 +0100

    :art: Update Codebase
    Update readme file and added check for non-exisiting morse characters

--- decoder.rb
+++ decoder.rb
@@ -3,16 +3,16 @@ require_relative "dictionary.rb"
 include Dictionary
 
 def decode_char(morse)
-    message = ''
-    words = morse.split('   ')
-    words.each do |word|
-       chars = word.split(' ')
-       chars.each do |char|
-            message += Dictionary.morse_dictionary(char)
-       end
-       message += ' '
-     end
-    return message;
+  message = ''
+  words = morse.split('   ')
+  words.each do |word|
+    chars = word.split(' ')
+    chars.each do |char|
+      message += Dictionary.morse_dictionary(char) || ' '
+    end
+    message += ' '
+  end
+  return message;
 end
 
 puts("Enter your morse message: ")
